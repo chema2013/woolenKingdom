190c5003d97cf9c7018b51b01e6d623522ea59e4
Sunaina Miller <sunaina.miller@ue-germany.de>
2022-04-13 18:25:10 +0200

    Added working character controller

--- WoollenKingdom/Assets/Scripts/PlayerMovement.cs
+++ WoollenKingdom/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float horizontalMovement;
+    float verticalMovement;
+    public Rigidbody2D rb;
+    public float speed = 2f;
+    Vector2 movement;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(horizontalMovement, verticalMovement);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
     }
 }
