d1303ca12e5fc81e4c4f74417c315a94a89ed749
Sunaina Miller <sunaina.miller@ue-germany.de>
2022-04-20 17:43:57 +0200

    Basic player movement done

---
+++ WoollenKingdom/Assets/Scripts/PlayerScripts/IsoPlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsoPlayerMovement : MonoBehaviour
+{
+    float horizontalMovement;
+    float verticalMovement;
+    public Rigidbody2D rb;
+    public float speed = 2f;
+    Vector2 movement;
+
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(horizontalMovement, verticalMovement);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
+    }
+}
