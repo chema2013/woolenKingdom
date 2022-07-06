6fcc265880f273e343171f5ab147d4a9db905b38
Sunaina Miller <sunaina.miller@ue-germany.de>
2022-07-06 15:29:34 +0200

    Added Manager with quit function to all game levels

--- WoollenKingdom/Assets/Scripts/MenuScripts/PauseMenu.cs
+++ WoollenKingdom/Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -5,52 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public static bool GameIsPaused = false; //check from other scripts (and not this specific script) if the game is paused
-    public GameObject pauseMenuUI;
-
-    void Start()
-    {
-        pauseMenuUI.SetActive(false);
-    }
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
+            QuitGame();
         }
     }
 
-    public void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
-        Debug.Log("Game resumed");
-    }
-
-    public void menu()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(0);
-    }
-
-    void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPaused = true;
-        Debug.Log("Game paused");
-    }
-
-    public void QuitGame()
+    void QuitGame()
     {
         Application.Quit();
         Debug.Log("Game quit");
